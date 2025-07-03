656fa87b5775b7fd7a5ed9eca595bbfa57605077
Sam Bell <sambo@asab.us>
2025-07-02 21:02:13 -0400

    works

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,12 @@
 #include "inky.h"
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <iostream>
+#include <cassert>
+#include <iomanip>
 
 
+using namespace std;
 
 int main(int argc, char**argv)
 {
@@ -14,5 +18,11 @@ int main(int argc, char**argv)
 
 std::ifstream f("thefile.json");
 json data = json::parse(f);
+
+string s = data.dump();
+
+cout << "serialization with pretty printing: " << data.dump(4) << endl;
+
+
     printf("End Parsing\n");
 }
