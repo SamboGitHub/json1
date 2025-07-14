0900719586ac3cdb1eaec690b63c115a1b9d199b
John Maxwell <jmax@jmaxhome.com>
2025-07-14 19:09:28 -0400

    Pretty print dump the entire file.
    Also dump the parameter names and IDs.

--- src/main.cc
+++ src/main.cc
@@ -64,31 +64,46 @@ for (json::iterator it = data.begin(); it != data.end(); ++it)
 std::cout << "Parse the Array \n";
 //   dumps all
 //    std::cout << it.key() << " : " << it.value() << "\n";
-std::cout << result_array.dump() << "\n \n \n"; 
+// std::cout << result_array.dump() << "\n \n \n"; 
 // std::cout << result_array[1] << "\n"; 
+// 
 
-json detail = json::parse(valstring);
+// the (4) makes it pretty-print.
+ std::cout << result_array.dump(4) << "\n";
+
+ // dump the ids
+ std::cout << "IDs" << "\n";
+ for (json::iterator it = result_array.begin(); it != result_array.end(); ++it) {
+   // skip anything that isn't an object
+   if (!it->is_object())
+     continue;
+   std::cout << "name: " << (*it)["Parameter"]["name"]
+             << ";  id: " << (*it)["Parameter"]["id"]
+             << "\n";
+ }
+
+ //json detail = json::parse(valstring);
 // json detail = json::parse(result_array);
 std::cout << "Done Parse the Parse \n";
 
-for (json::iterator that = detail.begin(); that != detail.end(); ++that)
-{
+// for (json::iterator that = detail.begin(); that != detail.end(); ++that)
+// {
 
-    std::cout << " - " ;
-    keystring = that.key();
+//     std::cout << " - " ;
+//     keystring = that.key();
 
-    if(keystring.find("result") == 0)
-    {
+//     if(keystring.find("result") == 0)
+//     {
         
-        std::cout << that.key();
-        // std::cout << " : ";
-        std::cout << that.value();
-        std::cout << "\n";
-        valstring = that.value();
-    }
-    //   dumps all
-    //    std::cout << it.key() << " : " << it.value() << "\n";
-}
+//         std::cout << that.key();
+//         // std::cout << " : ";
+//         std::cout << that.value();
+//         std::cout << "\n";
+//         valstring = that.value();
+//     }
+//     //   dumps all
+//     //    std::cout << it.key() << " : " << it.value() << "\n";
+// }
 
 
 
